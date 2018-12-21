d1a39c5062ccb49524ae43571904b8d28d095a00
guoshencheng <648772021@qq.com>
2018-12-21 19:42:21 +0800

    fix size control

--- example/index.js
+++ example/index.js
@@ -2,12 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { chars } from '../lib/index.js';
 
+const names = ['C', 'e', 'n', 't', 'u', 'r', 'y'];
+const world = ['W', 'o', 'r', 'l', 'd'];
+const posts = ['p', 'o', 's', 't', 's'];
+const github = ['g', 'i', 't', 'h', 'u', 'b'];
+
+const PressStart2PString = ({ str, ...args }) => (
+  <div {...args}>
+    {
+      str.split('').map((c, index) => {
+        const Comp = chars[c];
+        return <Comp size={40} key={index} />
+      })
+    }
+  </div>
+);
+
 const App = (
   <div className="app">
+    <PressStart2PString str="Century" />
+    <PressStart2PString str="world" />
+    <PressStart2PString str="posts" />
+    <PressStart2PString str="github" />
     {
       Object.keys(chars).map(key => {
         const Comp = chars[key];
-        return <Comp key={key} />
+        return <Comp size={40} key={key} />
       })
     }
   </div>
